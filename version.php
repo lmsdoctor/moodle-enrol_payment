e77fef4011d79be10c844346e61dc7a382076f1e
Andrés Ramos <andres.ramos@lmsdoctor.com>
2020-02-13 13:21:25 -0500

    Update version.php

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020023300;
+$plugin->version   = 2020023301;
 $plugin->requires  = 2018120300;
 $plugin->component = 'enrol_payment';
 $plugin->maturity  = MATURITY_RC;
-$plugin->release   = '1.1.3';
+$plugin->release   = '1.1.4';
