0a82a6a115a8103f68fb5df76917e0fc33d98f83
Andrés Ramos <andres.ramos@lmsdoctor.com>
2020-02-12 20:56:23 -0500

    Bump version file

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020021100;
+$plugin->version   = 2020021200;
 $plugin->requires  = 2018120300;
 $plugin->component = 'enrol_payment';
 $plugin->maturity  = MATURITY_RC;
-$plugin->release   = '1.1.0';
+$plugin->release   = '1.1.1';
